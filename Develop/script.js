6f8a7cd334f8677da741abc1a1d9b44630a8d2ab
Steph Heins <stephanie_heins@yahoo.com>
2021-07-07 15:26:06 -0700

    Added second function, cleaned it up.

--- Develop/script.js
+++ Develop/script.js
@@ -17,6 +17,8 @@ generateBtn.addEventListener("click", writePassword);
 
 */
 
+
+
 // Variables 
 
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
@@ -24,28 +26,76 @@ var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
 var numericalChar = "0123456789";
 var specialChar = "!#$%&'()*+-./:;<=>?@[_`{|}~";
 
+var passwordLengthInput = "";
+
 // Password Length 
 
 function generatePassword() {
 
-  var passwordLengthInput = prompt("Choose a password length between 8 - 128 characters:")
+  passwordLengthInput = prompt("Choose a password length between 8 - 128 characters:")
   passwordLengthInput = parseInt(passwordLengthInput);
+
+  //Prompt if entry is smaller than 8
   if (passwordLengthInput < 8) 
       { alert ("Password must have more than 7 characters!");
-      return prompt("Choose a password length between 8-128 characters:");
+      prompt("Choose a password length between 8-128 characters:");
       }
+  
+  // Prompt if entry is larger than 128
   if (passwordLengthInput > 128) 
       { alert ("Password must have less than 128 characters!");
-      return prompt("Choose a password length between 8-128 characters:");
+      prompt("Choose a password length between 8-128 characters:");
       }
 
+  // Prompt if entry is not a number
+  if (isNaN(passwordLengthInput))
+      { alert ("Please enter a numeric value.");
+      prompt("Choose a password length between 8-128 characters:");
+      }
+
+  // Pass to next function if input meets requirements 
+  else if (passwordLengthInput > 8 && passwordLengthInput < 128)
+      { passwordCharacter();
+      }
 }
 
 // Password Characters 
 
 function passwordCharacter() {
 
-  var passwordCharInput = confirm ("")
+  var passwordChar = "";
+
+  var passwordLower = confirm ("Would you like to include lowercase letters?");
+    if (passwordLower) 
+    { passwordChar += lowercaseChar;
+    }
+
+  var passwordUpper = confirm ("Would you like to include UPPERCASE letters?");
+    if (passwordUpper) 
+    { passwordChar += uppercaseChar;
+    }
+
+  var passwordNumeric = confirm ("Would you like to include numbers?");
+    if (passwordNumeric)
+    { passwordChar += numericalChar;
+    }
+
+  var passwordSpecial = confirm ("Would you like to include special characters?")
+    if (passwordSpecial)
+    { passwordChar += specialChar;
+    }
+
+// Password Generate 
+
+var password = "";
+
+for (var i = 0; i < passwordLengthInput; i++ )
+{
+password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
+
+var pwTextArea = document.getElementById("password");
+pwTextArea.value = password;
 
 }
 
+}
